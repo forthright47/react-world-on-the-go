0a3e107af08ccacfb0ee2b3a4095a94e6106eba5
forthright47 <k.tuhinjaman@gmail.com>
2026-03-17 17:26:04 +0600

    implement count visited countries and show them in UI using list item

--- src/components/Countries/Countries.jsx
+++ src/components/Countries/Countries.jsx
@@ -1,19 +1,35 @@
-import React, { use } from 'react';
+import React, { use, useState } from 'react';
 import Country from '../Country/Country';
 import './Countries.css';
 
 const Countries = ({countriesPromise}) => {
+    const [visitedCountries, setVisitedCountries] = useState([]);
+    const handleVisitedCountries = (country) =>{
+        console.log('handleVisitedCountries clicked', country);
+        const newVisitedCountries = [...visitedCountries, country];
+        setVisitedCountries(newVisitedCountries);
+    }
     const countriesData = use(countriesPromise);
     const countries = countriesData.countries;
 
     return (
         <div>
             <h1>Total countries: {countries.length}</h1>
+            <p>Total visited countries: {visitedCountries.length}</p>
+            <ol>
+                {
+                    visitedCountries.map(country => <li
+                    key={country.cca3.cca3}
+                    >{country.name.common}</li>)
+                }
+            </ol>
             <div className='countries'>
                 {
                     countries.map(country => <Country
                     key={country.cca3.cca3}
-                    country={country}></Country>)
+                    country={country}
+                    handleVisitedCountries={handleVisitedCountries}
+                    ></Country>)
                 }
             </div>
         </div>
--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import './Country.css';
 
-const Country = ({country}) => {
+const Country = ({country, handleVisitedCountries}) => {
 
     const [visited, setVisited] = useState(false);
 
@@ -12,6 +12,7 @@ const Country = ({country}) => {
         else{
             setVisited(true);
         }
+        handleVisitedCountries(country);
     }
 
     return (
